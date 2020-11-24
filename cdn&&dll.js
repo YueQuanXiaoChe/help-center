db8fa504ce230471a58cbf038b86349ca12caa83
wangxue <312759057@qq.com>
2020-11-24 17:51:19 +0800

    update

--- cdn&&dll.js
+++ cdn&&dll.js
@@ -1,8 +1,12 @@
-// const packageJson = require("./package.json");
+const packageJson = require("./package.json");
 
-// const getVersion = (param) => {
-//   return packageJson.dependencies[param].split("^")[1];
-// };
+const getVersion = param => {
+  return packageJson.dependencies[param].split("^")[1];
+};
+
+const jsdelivr = (key, value) => {
+  return `https://cdn.jsdelivr.net/npm/${key}@${getVersion(key)}/${value}`;
+};
 
 const externals = {
   vue: "Vue",
@@ -11,20 +15,32 @@ const externals = {
   axios: "axios"
 };
 
+const cdnMap = new Map([
+  ["vue", "dist/vue.min.js"],
+  ["vue-router", "dist/vue-router.min.js"],
+  ["vuex", "dist/vuex.min.js"],
+  ["axios", "dist/axios.min.js"]
+]);
+
+const dll = {};
+
 const cdn = {
   css: [],
   js: [
-    // vue
-    "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
-    // vue-router
-    "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
-    // vuex
-    "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
-    // axios
-    "https://cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js"
-    // core-js
-    // "https://cdn.jsdelivr.net/npm/core-js@3.6.5/index.min.js"
+    // // vue
+    // "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
+    // // vue-router
+    // "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
+    // // vuex
+    // "https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
+    // // axios
+    // "https://cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js"
   ]
 };
 
-exports = module.exports = { externals, cdn };
+Object.entries(externals).map(entry => {
+  cdn.js.push(jsdelivr(entry[0], cdnMap.get(entry[0])));
+});
+
+
+exports = module.exports = { externals, cdn, dll };
